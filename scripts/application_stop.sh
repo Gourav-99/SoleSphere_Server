669c781dedeb88f131c5f028367a37460973c8b8
Gourav <goupg1999@gmail.com>
2023-09-09 16:39:16 +0530

    code deplooy failed

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,7 +2,7 @@
 echo "Stopping any existing servers"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 pkill node
