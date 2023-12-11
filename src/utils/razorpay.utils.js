852a39d4698764c74fc203fa42378a03d5f46c0a
GOURAV PURI GOSWAMI <52204053+Gourav-99@users.noreply.github.com>
2023-12-11 13:03:41 +0530

    Update razorpay.utils.js

--- src/utils/razorpay.utils.js
+++ src/utils/razorpay.utils.js
@@ -1,6 +1,6 @@
 import Razorpay from "razorpay";
 
 export const razorpayInstance = new Razorpay({
-  key_id: process.env.RAZOR_API_ID || "rzp_test_M08pbaHX8ZXrUY",
-  key_secret: process.env.RAZOR_SECRET_KEY || "rzp_test_M08pbaHX8ZXrUY",
+  key_id: process.env.RAZOR_API_ID,
+  key_secret: process.env.RAZOR_SECRET_KEY ,
 });
