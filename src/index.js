0ee0f5ccdf12eceb75f974cefd3cc9a31da4b44f
GOURAV PURI GOSWAMI <52204053+Gourav-99@users.noreply.github.com>
2023-12-11 12:40:58 +0530

    Update index.js
    adding cors origin

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 connectDB();
-app.use(cors());
+app.use(cors({
+  origin: "https://sole-sphere-api.onrender.com",
+  credentials: true,
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morganMiddleware);
